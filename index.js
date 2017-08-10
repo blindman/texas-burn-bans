7c2a5cf5973051625bd8e93ca8a8078a59cdc519
blindman <jon.heller02@gmail.com>
2017-08-09 20:11:44 -0500

    add simpler formatting of data for usage by the callback

--- index.js
+++ index.js
@@ -37,8 +37,33 @@ const convertXmlToJson = (_xml, _successFn) => {
 	});
 };
 
+const formatJson = (_json, _successFn) => {
+	if (!_json) {
+		throw new Error('No JSON was provided.');
+	}
+
+	if (!_successFn) {
+		throw new Error('No callback was provided to handle the data.');
+	}
+
+	_successFn({
+		source: ENDPOINT,
+		title: _json.rss.channel[0].title[0],
+		info: _json.rss.channel[0].link[0],
+		map: _json.rss.channel[0].item[0].link[0],
+		counties: _json.rss.channel[0].item[0].description[0].split(', '),
+		author: _json.rss.channel[0].item[0].author[0],
+		date: new Date(_json.rss.channel[0].item[0].title[0].split(' - ')[1].trim())
+	});
+};
+
 module.exports = _successFn => {
+	if (!_successFn) {
+		throw new Error('No callback was provided to handle the data (this operation is synchronous only in its current version).');
+	}
 	retrieveData(xmlData => {
-		convertXmlToJson(xmlData, _successFn);
+		convertXmlToJson(xmlData, jsonData => {
+			formatJson(jsonData, _successFn);
+		});
 	});
 };
