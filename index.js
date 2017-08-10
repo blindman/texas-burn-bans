da0cacc8af3fa9f79ad4d7c05f1aeebcfce21d03
blindman <jon.heller02@gmail.com>
2017-08-09 19:07:16 -0500

    implement request to retrieve the data

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 'use strict';
-module.exports = () => {
+const request = require('request');
 
+const ENDPOINT = 'http://tfsfrp.tamu.edu/wildfires/BurnBan.xml';
+
+const retrieveData = _successFn => {
+	if (!_successFn) {
+		throw new Error('No callback was provided to handle the data.');
+	}
+
+	const requestOptions = {method: 'GET', uri: ENDPOINT, encoding: 'utf16le'};
+	request(requestOptions, (error, response, body) => {
+		if (error) {
+			throw new Error(error);
+		}
+
+		_successFn(body);
+	});
 };
+
+module.exports = _successFn => retrieveData(_successFn);
